75eceb9ec0618d479223c059f601a174556cb864
Georgiy Podobin <gpodobin@yandex.ru>
2026-03-04 21:45:12 +0300

    Add query for statistics by responses

---
+++ get_responses_statistics.sql
@@ -0,0 +1,14 @@
+WITH filtered_responses AS (
+    SELECT r.response_id, v.vacancy_id, v.title
+    FROM responses AS r
+    INNER JOIN vacancies AS v ON v.vacancy_id = r.vacancy_id
+    WHERE r.response_date - v.publication_date <= 7
+),
+grouped_vacancies AS (
+    SELECT vacancy_id, title, count(response_id) 
+    FROM filtered_responses
+    GROUP BY vacancy_id, title
+)
+SELECT vacancy_id, title, count
+FROM grouped_vacancies
+WHERE count >= 5;
