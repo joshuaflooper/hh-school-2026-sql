80d5a4dfaf683658e66b0bd40100461cb38c3cd6
Georgiy Podobin <gpodobin@yandex.ru>
2026-03-04 13:31:01 +0300

    Change sctructure of tables

--- create_tables.sql
+++ create_tables.sql
@@ -8,19 +8,6 @@ CREATE TYPE payment_frequency_type AS ENUM (
 	'for_a_project'
 );
 
-CREATE TYPE work_schedule_type AS ENUM (
-	'6/1', '5/2', '4/3', '4/2', '3/3', '3/2', '2/2', '2/1', '1/3', '1/2', '4/4',
-	'flexible',
-	'on_weekends',
-	'other'
-);
-
-CREATE TYPE working_hours_type AS ENUM (
-	'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '24', 
-	'by_agreement', 
-	'other'
-);
-
 CREATE TYPE gender_type AS ENUM ('male', 'female');
 
 CREATE TABLE employers (
@@ -59,14 +46,11 @@ CREATE TABLE vacancies (
 	area_id integer NOT NULL REFERENCES areas(area_id),
     specialization_id integer NOT NULL REFERENCES specializations(specialization_id),
 	title text NOT NULL,
-	keywords text[],
 	description text NOT NULL,
 	required_experience required_experience_type NOT NULL,
 	salary_from integer,
 	salary_to integer,
 	payment_frequency payment_frequency_type NOT NULL,
-	work_schedule work_schedule_type[] NOT NULL,
-	working_hours working_hours_type[] NOT NULL,
 	publication_date date DEFAULT now()
 );
 
@@ -76,19 +60,15 @@ CREATE TABLE resumes (
 	area_id integer NOT NULL REFERENCES areas(area_id),
 	specialization_id integer NOT NULL REFERENCES specializations(specialization_id),
 	title text NOT NULL,
-	keywords text[] NOT NULL,
 	description text NOT NULL,
 	experience integer NOT NULL,
 	salary_from integer,
-	salary_to integer,
-	work_schedule work_schedule_type,
-	working_hours working_hours_type,
 	publication_date date DEFAULT now()
 );
 
 CREATE TABLE responses (
 	response_id serial PRIMARY KEY, 
-	user_id integer NOT NULL,
+	resume_id integer NOT NULL,
 	vacancy_id integer NOT NULL,
 	covering_letter text,
 	response_date date DEFAULT now()
