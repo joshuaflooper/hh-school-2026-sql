42eed290cfa1374e6e74d63a4e61dbd14b6c935c
Georgiy Podobin <gpodobin@yandex.ru>
2026-03-05 16:52:36 +0300

    Fix some logic

--- get_vacancies_and_resumes_statistics.sql
+++ get_vacancies_and_resumes_statistics.sql
@@ -1,16 +1,3 @@
-(WITH formatted_date_resumes(resume_id, month, year) AS (
-    SELECT 
-        resume_id,
-        extract(month FROM publication_date) AS month,
-        extract(year FROM publication_date) AS year
-    FROM resumes
-)
-SELECT 'resumes' AS type, month, year, count(resume_id)
-FROM formatted_date_resumes
-GROUP BY month, year
-ORDER BY count DESC
-LIMIT 1)
-UNION
 (WITH formatted_date_vacancies(vacancy_id, month, year) AS (
     SELECT 
         vacancy_id,
@@ -20,6 +7,19 @@ UNION
 )
 SELECT 'vacancies' AS type, month, year, count(vacancy_id)
 FROM formatted_date_vacancies
+GROUP BY month, year
+ORDER BY count DESC
+LIMIT 1)
+UNION
+(WITH formatted_date_resumes(resume_id, month, year) AS (
+    SELECT 
+        resume_id,
+        extract(month FROM publication_date) AS month,
+        extract(year FROM publication_date) AS year
+    FROM resumes
+)
+SELECT 'resumes' AS type, month, year, count(resume_id)
+FROM formatted_date_resumes
 GROUP BY month, year
 ORDER BY count DESC
 LIMIT 1);
